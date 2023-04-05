ed32f0e840a1b6d716ba2548f1c9186797238ba2
HueJazz <alexkorystin@ukr.net>
2023-04-06 00:04:01 +0300

    added lab4 task1,2

--- lab4/task2_3.js
+++ lab4/task2_3.js
@@ -2,12 +2,10 @@ const tooltipContainer = document.querySelector("#tooltipContainer");
 const inputForm = document.querySelector("#tooltipInput");
 const inputButton = document.querySelector("#changeTooltip");
 
-// додати обробник події на кнопку "Змінити підказку"
 inputButton.addEventListener("click", () => {
   createTooltip(inputForm.value);
 });
 
-// функція для створення нової підказки
 const createTooltip = (text) => {
   const newTooltip = document.createElement("div");
   newTooltip.classList.add("tooltip");
@@ -15,7 +13,6 @@ const createTooltip = (text) => {
   tooltipContainer.appendChild(newTooltip);
 };
 
-// додати обробник події на елемент, до якого хочемо додати підказку
 const myButton = document.querySelector("#myText");
 myButton.addEventListener("mouseenter", () => {
   const tooltipText = tooltipContainer.lastChild.innerText;
